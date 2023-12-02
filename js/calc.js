8107af4c7f76ea81f6075c8043e686d8c19cb131
OlegPeretykin <oleg.peretykin@mailhost.email>
2023-12-02 12:52:36 +0000

    Before write calc logic

---
+++ js/calc.js
@@ -0,0 +1,16 @@
+function calc() {
+/*     var type_design = document.getElementById("type_design").value;
+    var is_html = document.getElementById("is_html").checked;
+    var count = +document.getElementById("count").value;
+    var result = document.getElementById("result"); 
+  
+    var price = 0;
+    price += +type_design;
+    price += is_html ? +document.getElementById("is_html").value : 0;
+    price *= count;
+  
+    result.innerHTML = price;
+    test.innerHTML = document.getElementById("is_html").value; */
+
+    
+  }
